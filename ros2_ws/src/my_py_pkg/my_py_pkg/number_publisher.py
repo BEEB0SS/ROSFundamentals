d2c50c135316fbd5a99d872ca8f80a445c116ba3
Pratham Madaram <madarampratham@gmail.com>
2026-05-23 12:10:34 -0400

    publisher and timer

--- ros2_ws/src/my_py_pkg/my_py_pkg/number_publisher.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -1,11 +1,20 @@
 import rclpy
 from rclpy.node import Node
+from example_interfaces.msg import Int64
 
 #Creating new class that inherits from the rclpy Node class
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__('number_publisher')
+        self.number_ = 2
+        self.number_publisher_ = self.create_publisher(Int64, 'number', 10)
+        self.number_timer_ = self.create_timer(1.0, self.publish_number)
+        self.get_logger().info('Number Publisher Node has been started.')
 
+    def publish_number(self):
+        msg = Int64()
+        msg.data = self.number_
+        self.number_publisher_.publish(msg)
 
 def main(args=None):
     #Initialize communications
